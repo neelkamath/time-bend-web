8951a929a9fa5dd28daf6836a676449d339af152
neelkamath <neelkamath@icloud.com>
2020-03-17 13:19:12 +0530

    Use importScripts()

--- public/sw.ts
+++ public/sw.ts
@@ -1,6 +1,6 @@
-// This file must be in the public directory because the service worker's scope needs to be the site's root.
+// In order for the service worker to have the scope of the root directory, it must be placed in this directory.
 
-import {registerRoute} from 'workbox-routing';
-import {StaleWhileRevalidate} from 'workbox-strategies';
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
-registerRoute(/\.(?:html|css|js|png|svg|webmanifest)$/, new StaleWhileRevalidate());
+// @ts-ignore: Property '__WB_MANIFEST' does not exist on type 'Window & typeof globalThis'.
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
---
+++ src/workboxBuild.ts
@@ -0,0 +1,14 @@
+const workboxBuild = require('workbox-build');
+
+workboxBuild.injectManifest({
+    swSrc: 'public/sw.ts',
+    swDest: 'dist/sw.js',
+    globDirectory: 'dist',
+    globPatterns: ['**\/*.{js,css,html,png,webmanifest,svg}']
+}).then(
+    // @ts-ignore: Binding element 'size' implicitly has an 'any' type.
+    ({count, size, warnings}) => {
+        warnings.forEach(console.warn);
+        console.log(`${count} files will be precached, totaling ${size} bytes.`);
+    }
+);
